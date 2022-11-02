74114f21b062278b3355cea28e49bb4cac7f8ae4
grostaco <grostaco@gmail.com>
2022-11-02 15:37:22 +0700

    feat: add nohari

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,2 +1,3 @@
 pub mod johari;
+pub mod nohari;
 pub mod util;
---
+++ src/commands/nohari.rs
@@ -0,0 +1,7 @@
+use soshari_macros::generate_adjectives;
+
+generate_adjectives! { incompetent, intolerant, inflexible, timid, cowardly, violent, aloof, glum, stupid, simple,
+insecure, irresponsible, vulgar, lethargic, withdrawn, hostile, selfish, unhappy, unhelpful,
+cynical, needy, unimaginative, inane, brash, cruel, ignorant, irrational, distant, childish, boastful,
+blase, imperceptive, chaotic, impatient, weak, embarrassed, loud, vacuous, panicky, unethical, insensitive,
+self_satisfied, passive, smug, rash, dispassionate, overdramatic, dull, predictable, callous, inattentive, unreliable, cold, foolish, humourless }
